da57e9916f975d882555721071628c2db883ba1e
lazaroah <lazaroah02@gmail.com>
2024-09-06 17:51:57 -0400

    evoiding downloading .db fildes

--- utils/constants.py
+++ utils/constants.py
@@ -15,7 +15,7 @@ FILE_EXTENSIONS = [
     # Documentos
     '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt', '.rtf', '.odt', '.ods', '.odp', '.srt',
     # Archivos comprimidos y de imagen de disco
-    '.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz', '.iso', '.dmg',  
+    '.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz', '.iso', '.dmg', '.db', 
     # Archivos de código y scripts
     '.html', '.htm', '.css', '.js', '.json', '.xml', '.csv', '.sql', '.php', '.asp', '.aspx', 
     '.jsp', '.py', '.rb', '.java', '.c', '.cpp', '.h', '.hpp', '.cs', '.swift', '.go', '.sh', '.bat', '.cmd', 
