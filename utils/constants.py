5cff63c37380768a3f312337fe6737f8bcf340f9
lazaroah <lazaroah02@gmail.com>
2024-09-06 22:00:07 -0400

    adding files extensions that was not included

--- utils/constants.py
+++ utils/constants.py
@@ -1,28 +1,42 @@
 ALLOWED_FORMATS = [
     "avi", "srt", "mkv", "mp4", "mpg",
     "mov", "wmv", "flv", "webm", "3gp",
-    "vtt", "sub", "ass", "ssa", "vob"
+    "vtt", "sub", "ass", "ssa", "vob",
+    "mpeg", "divx", "ifo", "bup" 
 ]
+
 
 
 FILE_EXTENSIONS = [
     # Imágenes
-    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp',  
+    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp',  '.pcx', '.tga',  
+
     # Videos
-    '.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.mpg', '.mpeg', '.3gp', '.vob', '.divx', 
+    '.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.mpg', '.mpeg', '.3gp', '.vob', '.divx', '.ifo', '.bup', 
+    '.asf', '.rm', '.swf',  
+
     # Audios
     '.mp3', '.wav', '.aac', '.ogg', '.m4a', '.flac', '.wma', '.aiff', '.alac',  
+    '.mid', '.midi', '.ra',  
+
     # Documentos
     '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt', '.rtf', '.odt', '.ods', '.odp', '.srt',
+    '.wpd', '.tex', '.wps',  
+
     # Archivos comprimidos y de imagen de disco
     '.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz', '.iso', '.dmg', '.db', 
+    '.cab', '.lzh',  
+
     # Archivos de código y scripts
     '.html', '.htm', '.css', '.js', '.json', '.xml', '.csv', '.sql', '.php', '.asp', '.aspx', 
     '.jsp', '.py', '.rb', '.java', '.c', '.cpp', '.h', '.hpp', '.cs', '.swift', '.go', '.sh', '.bat', '.cmd', 
+    '.pl', '.vb',  
+
     # Archivos ejecutables y de sistema
-    '.exe', '.dll', '.bin', '.sys', '.msi', '.apk', '.deb', '.rpm',  
+    '.exe', '.dll', '.bin', '.sys', '.msi', '.apk', '.deb', '.rpm',  '.com', '.drv',  
+
     # Otros
-    '.nfo', '.ts', '.m3u8', '.log', '.cfg', '.ini', '.inf', '.plist', 
+    '.nfo', '.ts', '.m3u8', '.log', '.cfg', '.ini', '.inf', '.plist', '.bak', '.tmp',  
 ]
 
 DATABASE_DIRECTORY = "./remembered_database"
